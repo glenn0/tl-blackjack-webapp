8ce50dc1bbe2369dbfb1525c5d18851fbb1dfc20
Glenn <glenn@fil.com.au>
2013-03-23 18:19:09 +1100

    Checks for username in session, redirects to form.

--- main.rb
+++ main.rb
@@ -5,9 +5,24 @@ require 'shotgun'
 set :sessions, true
 
 get '/' do
-  erb :new_user_form
+  if session[:username]
+  	redirect '/signup'
+  else
+  	redirect '/game'
+  end
 end
 
-post '/myaction' do
-  puts params['username'], params['date'], params['password']
+get '/signup' do
+	erb :new_user_form
+end
+
+post '/saveuser' do
+  session[:username] = params['username']
+  puts params['username']
+  redirect '/game'
+end
+
+get '/game' do
+  puts "This is in /game."
+  puts session[:username]
 end
